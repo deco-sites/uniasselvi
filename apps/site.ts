4bf21ca14964b2e24e7935e45acd8fcb711d73a1
Giovanny Lisboa <162643577+glisboapuc@users.noreply.github.com>
2024-07-05 10:00:58 -0300

    alteracao do endpoint de cursos

--- apps/site.ts
+++ apps/site.ts
@@ -7,7 +7,7 @@ import { API } from "site/apps/api.ts";
 type WebsiteApp = ReturnType<typeof website>;
 
 export const api = createHttpClient<API>({
-  base: "https://www.uniasselvi.com.br",
+  base: "https://www.asselvi.net.br",
 });
 
 /**
